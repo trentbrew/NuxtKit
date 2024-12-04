4489aeb38f5743da7dcf6dbbf93466c93a43b863
Trent Brew <tbrew1023@gmail.com>
2024-12-03 17:02:43 -0800

    genesis

---
+++ layouts/blank.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const store = useNavbar()
+const { theme, radius } = useCustomize()
+
+useServerHead({
+  bodyAttrs: {
+    class: `theme-${theme.value}`,
+    style: `--radius: ${radius.value}rem;`,
+  },
+})
+
+const router = useRouter()
+
+defineShortcuts({
+  'Meta_B': () => store.toggle(),
+  'G-H': () => router.push('/'),
+  'G-E': () => router.push('/email'),
+})
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
