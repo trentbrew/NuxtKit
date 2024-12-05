fa763e9437b3b0ff617ea184a80ba8202b9e3726
Trent Brew <tbrew1023@gmail.com>
2024-12-04 18:26:19 -0800

    feat(ui): add missing pages

---
+++ pages/components/nodes/SpecialNode.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  position: {
+    type: Object,
+    required: true,
+  },
+});
+
+const x = computed(() => `${Math.round(props.position.x)}px`);
+const y = computed(() => `${Math.round(props.position.y)}px`);
+</script>
+
+<template>
+  <div class="vue-flow__node-default">
+    <div>{{ data.label }}</div>
+
+    <div>{x} {y}</div>
+
+    <Handle type="source" :position="Position.Bottom" />
+  </div>
+</template>
